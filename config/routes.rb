128e8cc7db4ce50cc5ce4e1c919f0455eac73aa4
t-kyab <hajsu00@gmail.com>
2022-01-02 17:11:18 +0900

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
-  get 'static_pages/about'
-  get 'static_pages/faq'
-  get 'static_pages/inquiry'
-  get 'static_pages/policy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "application#hello"
+  root "static_pages#top"
+  get  '/top',    to: 'static_pages#top'
+  get  '/about',   to: 'static_pages#about'
+  get  '/faq',   to: 'static_pages#faq'
+  get  '/inquiry', to: 'static_pages#inquiry'
+  get  '/policy', to: 'static_pages#policy'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,39 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  describe "GET /top" do
-    it "returns http success" do
-      get "/static_pages/top"
-      expect(response).to have_http_status(:success)
+  before do
+    @base_title = 'Hanger Talk'
+  end
+  context 'GET #root' do
+    before do
+      get root_path
+    end
+    it 'should respond top_page successfully' do
+      expect(response).to have_http_status 200
+    end
+    it 'has title: "Hanger Talk"' do
+      expect(response.body).to include "#{@base_title}"
     end
   end
 
-  describe "GET /about" do
-    it "returns http success" do
-      get "/static_pages/about"
-      expect(response).to have_http_status(:success)
+  context 'GET #about' do
+    before do
+      get about_path
+    end
+    it 'should respond successfully' do
+      expect(response).to have_http_status 200
+    end
+    it 'has title: "Hanger Talkとは？ | Hanger Talk"' do
+      expect(response.body).to include "Hanger Talkとは？ | #{@base_title}"
     end
   end
 
-  describe "GET /faq" do
-    it "returns http success" do
-      get "/static_pages/faq"
-      expect(response).to have_http_status(:success)
+  context 'GET #faq' do
+    before do
+      get faq_path
+    end
+    it 'should respond successfully' do
+      expect(response).to have_http_status 200
+    end
+    it 'has title: "よくある質問 | Hanger Talk"' do
+      expect(response.body).to include "よくある質問 | #{@base_title}"
     end
   end
 
-  describe "GET /inquiry" do
-    it "returns http success" do
-      get "/static_pages/inquiry"
-      expect(response).to have_http_status(:success)
+  context 'GET #inquiry' do
+    before do
+      get inquiry_path
+    end
+    it 'should respond successfully' do
+      expect(response).to have_http_status 200
+    end
+    it 'has title: "お問合せ | Hanger Talk"' do
+      expect(response.body).to include "お問合せ | #{@base_title}"
     end
   end
 
-  describe "GET /policy" do
-    it "returns http success" do
-      get "/static_pages/policy"
-      expect(response).to have_http_status(:success)
+  context 'GET #policy' do
+    before do
+      get policy_path
+    end
+    it 'should respond successfully' do
+      expect(response).to have_http_status 200
+    end
+    it 'has title: "プライバシーポリシー | Hanger Talk"' do
+      expect(response.body).to include "プライバシーポリシー | #{@base_title}"
     end
   end
-
 end
