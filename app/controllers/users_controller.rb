2b51e036b816ede42fdc58fdb81cc9c51e16b9a2
t-kyab <hajsu00@gmail.com>
2022-01-11 20:33:25 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "ユーザー登録に成功しました！"
       redirect_to @user
     else
-      render 'new'
+      flash[:danger] = "ユーザー登録に失敗しました。入力内容を確認してください。"
+      redirect_to new_user_path
     end
   end
 
