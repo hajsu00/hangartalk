76760a50503f851e5b442b76a704e22173b0a5d5
t-kyab <hajsu00@gmail.com>
2022-01-14 12:55:19 +0900

    Finish 10.2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       flash.now[:success] = 'ログインに成功しました！'
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'ログインに失敗しました。入力内容を確認してください。'
       render 'new'
