bb84275d7e61bc5674be1d5147b055944d085fca
t-kyab <hajsu00@gmail.com>
2022-01-15 14:38:15 +0900

    Finish 13.4.3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def top
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed
+    end
   end
 
   def about
