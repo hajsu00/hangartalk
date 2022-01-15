0ad7e6bb949828ff353c250573bcacb637159958
t-kyab <hajsu00@gmail.com>
2022-01-15 11:25:08 +0900

    Add password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "アカウントの有効化 | Hanger Talk"
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "Password reset"
+    mail to: user.email, subject: "Hanger Talk |  パスワード再設定のご案内"
   end
 end
