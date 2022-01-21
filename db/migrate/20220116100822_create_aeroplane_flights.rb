35bdf3ce8eb7e2b2c65c427f70c26a1e2122437a
t-kyab <hajsu00@gmail.com>
2022-01-21 21:52:38 +0900

    finish showing flight list

--- db/migrate/20220116100822_create_aeroplane_flights.rb
+++ db/migrate/20220116100822_create_aeroplane_flights.rb
@@ -5,10 +5,20 @@ class CreateAeroplaneFlights < ActiveRecord::Migration[6.1]
       t.date :departure_date
       t.string :aeroplane_type
       t.string :aeroplane_ident
+      t.string :departure_point
+      t.string :arrival_point
+      t.string :exercises_or_maneuvers
+      t.integer :number_of_takeoff
+      t.integer :number_of_landing
       t.datetime :moving_time
       t.datetime :stop_time
       t.boolean :is_pic
+      t.boolean :is_dual
       t.boolean :is_cross_country
+      t.boolean :is_night_flight
+      t.boolean :is_hood
+      t.boolean :is_instrument
+      t.boolean :is_simulator
       t.boolean :is_instructor
       t.string :note
       t.references :user, null: false, foreign_key: true
