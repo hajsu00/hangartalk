93dfa52636afb0b6346428318003c0a6f56740f3
t-kyab <hajsu00@gmail.com>
2022-01-29 22:28:30 +0900

    Add new flight views

--- app/controllers/aeroplane_flights_controller.rb
+++ app/controllers/aeroplane_flights_controller.rb
@@ -1,6 +1,6 @@
 class AeroplaneFlightsController < ApplicationController
   include SessionsHelper
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user, only: [:create, :select, :destroy]
   before_action :correct_user,   only: :destroy
 
   def new
