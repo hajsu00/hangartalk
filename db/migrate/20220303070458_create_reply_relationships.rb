c59a0fc05110d68fde1895118f6a4c4fa8489816
t-kyab <hajsu00@gmail.com>
2022-03-06 10:31:30 +0900

    Fixing << method error

--- db/migrate/20220303070458_create_reply_relationships.rb
+++ db/migrate/20220303070458_create_reply_relationships.rb
@@ -1,13 +1,18 @@
 class CreateReplyRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :reply_relationships do |t|
-      t.integer :replying_id
-      t.integer :replied_id
+      # t.integer :replying_id
+      # t.integer :replied_id
+      t.references :replying, null: false, foreign_key: { to_table: :microposts }
+      t.references :replied, null: false, foreign_key: { to_table: :microposts }
 
       t.timestamps
       end
-      add_index :reply_relationships, :replying_id
-      add_index :reply_relationships, :replied_id
+      # add_index :reply_relationships, :replying_id
+      # add_index :reply_relationships, :replied_id
+      # add_index :reply_relationships, [:replying_id, :replied_id]
+      # add_index :reply_relationships, :replying_id
+      # add_index :reply_relationships, :replied_id
       add_index :reply_relationships, [:replying_id, :replied_id]
   end
 end
