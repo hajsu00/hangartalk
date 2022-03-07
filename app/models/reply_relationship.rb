24061a1658c83aeefa32b85f05a38287c6182f82
t-kyab <hajsu00@gmail.com>
2022-03-07 11:19:11 +0900

    Adding unintended reply

--- app/models/reply_relationship.rb
+++ app/models/reply_relationship.rb
@@ -1,6 +1,6 @@
 class ReplyRelationship < ApplicationRecord
   belongs_to :replying, class_name: 'Micropost'
   belongs_to :replied, class_name: 'Micropost'
-  # validates :replying_id, presence: true
-  # validates :replied_id, presence: true
+  validates :replying_id, presence: true
+  validates :replied_id, presence: true
 end
