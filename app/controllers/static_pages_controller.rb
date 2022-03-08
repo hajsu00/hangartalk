c58cbb8c9f9671f9ac01af40a1ae974262105c30
t-kyab <hajsu00@gmail.com>
2022-03-09 07:09:03 +0900

    Finish sharing maicropost function

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ApplicationController
   def top
     if logged_in?
-      binding.pry
       @micropost  = current_user.microposts.build
       @microposts = current_user.feed.order(created_at: :desc).page(params[:page]).per(10)
     end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -25,7 +25,7 @@ class Micropost < ApplicationRecord
   has_many_attached :images
   #バリデーション
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }, if: :is_sharing_micropost?
+  validates :content, presence: true, length: { maximum: 140 }, unless: :is_sharing_micropost?
   validates :images,   content_type: { in: %w[image/jpeg image/gif image/png], message: "must be a valid image format" },
                       size: { less_than: 5.megabytes, message: "should be less than 5MB" }
   def display_image
