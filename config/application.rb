f45568d5b0216843612c8f0c08fab42a1ab33907
t-kyab <hajsu00@gmail.com>
2022-03-29 21:56:02 +0900

    Change app name

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module HangerTalkPrototype
+module HangerTalk
   class Application < Rails::Application
     config.load_defaults 6.1
     # ブラウザ側でJavaScriptが無効になっていた場合（Ajaxリクエストが送れない場合）でもうまく動くように
