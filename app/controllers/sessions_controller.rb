d0ca79cfadbf66736abdbbd274b3566d67b47d1f
t-kyab <hajsu00@gmail.com>
2022-03-31 10:02:30 +0900

    fixing login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,9 @@ class SessionsController < ApplicationController
   end
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+    binding.pry
     if user && user.authenticate(params[:session][:password])
+      binding.pry
       if user.activated?
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
@@ -15,7 +17,8 @@ class SessionsController < ApplicationController
         redirect_to root_url
       end
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      binding.pry
+      flash.now[:danger] = 'メールアドレスまたはパスワードが間違っています'
       render 'new'
     end
   end
