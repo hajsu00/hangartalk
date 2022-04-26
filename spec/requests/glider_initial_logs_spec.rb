81c7f3eba6511967201e91fe3b1975141cc27884
t-kyab <hajsu00@gmail.com>
2022-04-26 19:01:23 +0900

    [update]refs #24 remove old rspec code

--- spec/requests/glider_initial_logs_spec.rb
+++ spec/requests/glider_initial_logs_spec.rb
@@ -1,39 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "GliderInitialLogs", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/glider_initial_logs/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /new" do
+  #   it "returns http success" do
+  #     get "/glider_initial_logs/new"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/glider_initial_logs/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /create" do
+  #   it "returns http success" do
+  #     get "/glider_initial_logs/create"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/glider_initial_logs/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /show" do
+  #   it "returns http success" do
+  #     get "/glider_initial_logs/show"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/glider_initial_logs/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /edit" do
+  #   it "returns http success" do
+  #     get "/glider_initial_logs/edit"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/glider_initial_logs/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /update" do
+  #   it "returns http success" do
+  #     get "/glider_initial_logs/update"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
--- spec/systems/users_signup_spec.rb
+++ spec/systems/users_signup_spec.rb
@@ -1,39 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'users', type: :system do
-  describe 'user create a new account' do
-    context 'When valid values are entered' do
-      before do
-        visit signup_path
-        fill_in '名前', with: 'Example User'
-        fill_in 'メールアドレス', with: 'user@example.com'
-        fill_in 'パスワード', with: 'password'
-        fill_in 'パスワード（確認）', with: 'password'
-        click_button '認証メールを送信する'
-      end
-      it 'gets an flash message' do
-        expect(page).to have_selector('.alert-success', text: 'ユーザー登録に成功しました！')
-      end
-      it 'render to /users/(id) url' do
-        expect(current_path).to eq user_path(User.last)
-      end
-    end
-    context 'When invalid values are entered' do
-      before do
-        visit signup_path
-        fill_in '名前', with: ''
-        fill_in 'メールアドレス', with: 'user@invalid'
-        fill_in 'パスワード', with: ''
-        fill_in 'パスワード（確認）', with: ''
-        click_button '認証メールを送信する'
-      end
-      subject { page }
-      it 'gets an errors' do
-        is_expected.to have_selector('.alert-danger', text: 'ユーザー登録に失敗しました。入力内容を確認してください。')
-      end
-      it 'render to /signup url' do
-        is_expected.to have_current_path new_user_path
-      end
-    end
-  end
+  # describe 'user create a new account' do
+  #   context 'When valid values are entered' do
+  #     before do
+  #       visit signup_path
+  #       fill_in '名前', with: 'Example User'
+  #       fill_in 'メールアドレス', with: 'user@example.com'
+  #       fill_in 'パスワード', with: 'password'
+  #       fill_in 'パスワード（確認）', with: 'password'
+  #       click_button '認証メールを送信する'
+  #     end
+  #     it 'gets an flash message' do
+  #       expect(page).to have_selector('.alert-success', text: 'ユーザー登録に成功しました！')
+  #     end
+  #     it 'render to /users/(id) url' do
+  #       expect(current_path).to eq user_path(User.last)
+  #     end
+  #   end
+  #   context 'When invalid values are entered' do
+  #     before do
+  #       visit signup_path
+  #       fill_in '名前', with: ''
+  #       fill_in 'メールアドレス', with: 'user@invalid'
+  #       fill_in 'パスワード', with: ''
+  #       fill_in 'パスワード（確認）', with: ''
+  #       click_button '認証メールを送信する'
+  #     end
+  #     subject { page }
+  #     it 'gets an errors' do
+  #       is_expected.to have_selector('.alert-danger', text: 'ユーザー登録に失敗しました。入力内容を確認してください。')
+  #     end
+  #     it 'render to /signup url' do
+  #       is_expected.to have_current_path new_user_path
+  #     end
+  #   end
+  # end
 end
