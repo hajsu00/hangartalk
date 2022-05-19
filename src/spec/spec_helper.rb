395c2faa43ede51a50ac3a0aaeec1f837d44d11c
t-kyab <hajsu00@gmail.com>
2022-05-19 14:47:08 +0900

    [update]refs #21 fix capybara setting

--- src/spec/spec_helper.rb
+++ src/spec/spec_helper.rb
@@ -1,5 +1,20 @@
 RSpec.configure do |config|
   require 'pry'
+  require 'selenium-webdriver'
+  require 'capybara/rspec'
+
+  Capybara.configure do |capybara_config|
+    capybara_config.default_driver = :selenium_chrome
+    capybara_config.default_max_wait_time = 10
+  end
+  # Capybaraに設定したドライバーの設定をします
+  Capybara.register_driver :selenium_chrome do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('headless') # ヘッドレスモードをonにするオプション
+    options.add_argument('--disable-gpu') # 暫定的に必要なフラグとのこと
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  end
+  Capybara.javascript_driver = :selenium_chrome
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
