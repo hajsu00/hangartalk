d0dcc76c97bebcd9bad6e46047d65ae64cbb07ad
t_kyab <hajsu00@gmail.com>
2022-06-01 20:13:21 +0900

    [Feature/setting aws ses]AWS SESでアカウント認証機能を実装 (#38)
    * user authentication
    
    * [update]refs #37 finish adding aws ses mailer

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -254,6 +254,7 @@ ActiveRecord::Schema.define(version: 2022_04_14_104910) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
