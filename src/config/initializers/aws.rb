cf5fa7dbb5ca169172453a04d395b9e9ea84b003
t-kyab <hajsu00@gmail.com>
2022-06-02 20:25:48 +0900

    [update]refs #39 fix config/initializers/aws.rb

--- src/config/initializers/aws.rb
+++ src/config/initializers/aws.rb
@@ -1,17 +1,8 @@
 require 'aws-sdk'
 
-if Rails.application.credentials.db.present?
-  Aws::Rails.add_action_mailer_delivery_method(
-    :ses,
-    credentials: Aws::Credentials.new(Rails.application.credentials.aws[:access_key_id],
-                                      Rails.application.credentials.aws[:secret_access_key]),
-    region: 'ap-northeast-1'
-  )
-else
-  Aws::Rails.add_action_mailer_delivery_method(
-    :ses,
-    credentials: Aws::Credentials.new(AWS_ACCESS_KEY_ID,
-                                      AWS_SECRET_ACCESS_KEY),
-    region: 'ap-northeast-1'
-  )
-end
+Aws::Rails.add_action_mailer_delivery_method(
+  :ses,
+  credentials: Aws::Credentials.new(Rails.application.credentials.aws[:access_key_id],
+                                    Rails.application.credentials.aws[:secret_access_key]),
+  region: 'ap-northeast-1'
+)
