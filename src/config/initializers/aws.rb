9ba83975306e43c16beb20c8b2db12c9e5202aaa
t-kyab <hajsu00@gmail.com>
2022-06-02 20:11:39 +0900

    [update]refs #39 devide docker-compose.yml to devepment and production

--- src/config/initializers/aws.rb
+++ src/config/initializers/aws.rb
@@ -1,8 +1,17 @@
 require 'aws-sdk'
 
-Aws::Rails.add_action_mailer_delivery_method(
-  :ses,
-  credentials: Aws::Credentials.new(Rails.application.credentials.aws[:access_key_id],
-                                    Rails.application.credentials.aws[:secret_access_key]),
-  region: 'ap-northeast-1'
-)
+if Rails.application.credentials.db.present?
+  Aws::Rails.add_action_mailer_delivery_method(
+    :ses,
+    credentials: Aws::Credentials.new(Rails.application.credentials.aws[:access_key_id],
+                                      Rails.application.credentials.aws[:secret_access_key]),
+    region: 'ap-northeast-1'
+  )
+else
+  Aws::Rails.add_action_mailer_delivery_method(
+    :ses,
+    credentials: Aws::Credentials.new(AWS_ACCESS_KEY_ID,
+                                      AWS_SECRET_ACCESS_KEY),
+    region: 'ap-northeast-1'
+  )
+end
