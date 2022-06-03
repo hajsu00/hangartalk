c25efe1e7600f367fd7b8e94f1fe627e9d5cb93c
t_kyab <hajsu00@gmail.com>
2022-06-04 07:57:41 +0900

    [Feature/aws ci cd]Github ActionsによるCD構築 (#40)
    * [update]refs # 39 add cd.yml for github actions cd
    
    * [update]refs #39 devide docker-compose.yml to devepment and production
    
    * [update]refs #39 fix config/initializers/aws.rb
    
    * [update]refs #39 add envitonment viriable to Github Actions yml
    
    * [update]refs #39 add envinronment variable for RAILS_MASTER_KEY
    
    * [update]refs #39 fix env: RAOLS_MASTER_KEY
    
    * [update]refs #39 fix env
    
    * [update]refs #39 fix env
    
    * [update]refs #39 add env to docker-compose.production.yml
    
    * [update]refs #39 add env to docker-compose.production.yml
    
    * [update]refs #39 add env to docker-compose.production.yml
    
    * [update]refs #39 add env to docker-compose.production.yml
    
    * [update]refs #39 add env to docker-compose.production.yml
    
    * [update]refs #39 add ARG to Dockerfile of hangartalk_app
    
    * [update]refs #39 add ARG to Dockerfile of hangartalk_app
    
    * [update]refs #39 fix run command of workflows for CD
    
    * [update]refs #39 add trigger of github/workflows
    
    * [update]refs #39 fix some UI

--- src/config/environments/test.rb
+++ src/config/environments/test.rb
@@ -36,13 +36,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory.
   config.active_storage.service = :test
 
-  config.action_mailer.perform_caching = false
-
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
-
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
@@ -57,6 +50,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost', protocol: 'http' }
 end
