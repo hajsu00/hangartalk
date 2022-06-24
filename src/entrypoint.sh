d8c2c93419adcacfea32c905d371b19252b01fba
t_kyab <hajsu00@gmail.com>
2022-06-24 09:41:54 +0900

    Feature/rspec (#56)
    * [update]refs #54 fix header action UI
    
    * [update]refs #54 fix cd.yml for ecs deploy
    
    * [update]refs #54 set time_zone of RDS and refresh by db:reset
    
    * [update]refs #54 re-deploy with removing db:reset command from entrypoint.sh
    
    * [update]refs #54 fix README.md
    
    * [update]refs #54 fix cd.yml for ecs deploy

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -9,8 +9,5 @@ rm -f /hangartalk/tmp/pids/server.pid
 # bundle exec rails db:migrate RAILS_ENV=production
 # bundle exec rails db:seed RAILS_ENV=production
 
-# if [ "${RAILS_ENV}" = 'production' ]; then
-# fi
-
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
